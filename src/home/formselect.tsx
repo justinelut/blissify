6f66506588c86234dca0e3d04af9aeb7caa601ad
justinelut <justinequartz@gmail.com>
2024-01-10 16:14:06 +0300

    Designing, the single properties page

---
+++ src/home/formselect.tsx
@@ -0,0 +1,78 @@
+
+
+import {Link} from "@tanstack/react-router"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { toast } from "@/components/ui/use-toast"
+
+const FormSchema = z.object({
+  email: z
+    .string({
+      required_error: "Please select an email to display.",
+    })
+    .email(),
+})
+
+export function SelectForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl className="h-14">
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a verified email to display" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="m@example.com">m@example.com</SelectItem>
+                  <SelectItem value="m@google.com">m@google.com</SelectItem>
+                  <SelectItem value="m@support.com">m@support.com</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  )
+}
--- src/home/singleproperty.tsx
+++ src/home/singleproperty.tsx
@@ -1,17 +1,21 @@
+import { Card } from "@/components/ui/card";
 import { Heart, Share } from "lucide-react";
+import { CalendarForm } from "@/home/calender";
+import { SelectForm } from "./formselect";
+import { Button } from "@/components/ui/button";
 
 export default function SingleProperty() {
   return (
     <>
-      <div className="sticky top-0 py-10 bg-gray-200"></div>
+      <div className="sticky top-0 py-10 bg-white border-b"></div>
       <div className="mx-6 md:max-w-7xl md:mx-auto py-16">
         <div className="flex flex-row items-center justify-between">
           <div>
             <h4 className="font-semibold text-2xl">The Mabati Mansion</h4>
           </div>
           <div className="flex gap-4">
-            <Share />
-            <Heart />
+            <Share size={18} /> <p className="text-sm -ml-2">Share</p>
+            <Heart size={18} /> <p className="text-sm -ml-2">Save</p>
           </div>
         </div>
 
@@ -30,54 +34,30 @@ export default function SingleProperty() {
           </div>
         </div>
 
-        <div className="flex">
-          <div className="flex-grow w-2/3 bg-green-500 overflow-y-auto">
+        <div className="flex gap-10 items-start">
+          <div className="flex-grow w-2/3 overflow-y-auto">
+            <h1 className="text-3xl font-bold">Farm stay in Stellenbosch , South Africa</h1>
             <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
-            <h3>Entire place in Naivasha, Kenya</h3>
-            <h2>2 guests1 bedroom1 bed1 bath</h2>
+          
           </div>
-          <div className="flex-grow w-1/3 bg-red-500 max-h-40 sticky top-[14vh] justify-end">
-            <div className="shadow-xl top-0">
-              <h3>Ksh 400</h3>
+          <Card className="flex-grow-0 w-1/3 flex-shrink grid-cols- sticky top-[14vh] justify-end p-6 shadow-xl">
+            <div className="top-0">
+              <h3 className="font-semibold text-2xl">Ksh 400</h3>
+              <p>Total before taxes</p>
+
+              <div className="flex flex-col rounded-lg mt-4">
+                <div className="flex w-full justify-between">
+                   <CalendarForm />
+                   <CalendarForm />
+                </div>
+                <div className="w-full">
+                    <SelectForm />
+                </div>
+
+                <Button className="bg-green-500 p-4 mt-4">Reserve</Button>
+              </div>
             </div>
-          </div>
+          </Card>
         </div>
       </div>
     </>
